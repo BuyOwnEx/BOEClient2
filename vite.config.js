68e55ce54ca800d847f6d482a806d34233e0e06e
zns <have-dream@yandex.ru>
2024-07-19 01:31:42 +0300

    add vue, vuetify, i18n in vite config make jetstream install and migration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
+        }),
+        vue({
+            template: { transformAssetUrls },
+        }),
+        vuetify({
+            autoImport: true,
+            styles: { configFile: 'resources/scss/vuetify/settings.scss' },
+        }),
+        VueI18nPlugin({
+            include: 'resources/js/Translations/*'
         }),
     ],
 });
