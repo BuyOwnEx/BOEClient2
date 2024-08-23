099dffe7568546e5a0d4af65f30c7f1cbbffdb1d
BuyOwnEx <support@buyownex.com>
2024-08-23 04:11:40 +0300

    chart code optimization

--- resources/js/Components/Cards/TradingChart.vue
+++ resources/js/Components/Cards/TradingChart.vue
@@ -49,17 +49,6 @@ const props = defineProps({
     }
 });
 
-const intervals = [
-    { room: 1, range: '1m' },
-    { room: 5, range: '5m' },
-    { room: 15, range: '15m' },
-    { room: 30, range: '30m' },
-    { room: 60, range: '1h' },
-    { room: 240, range: '4h' },
-    { room: 1440, range: '1d' },
-    { room: 10080, range: '1w' },
-];
-
 const store = useStore();
 
 const hcInstance = ref(Highcharts);
@@ -72,24 +61,46 @@ const graph_loading = ref(false);
 let xData = props.ohlc;
 let yData = props.volume;
 
-/*const selectedPeriodObject = computed(() => {
-    return _.find(intervals, item => {
-        return item.range === candle_period.toLowerCase();
-    });
-})
-
-const candle_room = computed(() => {
-    return selectedPeriodObject.value.room;
-})
-
-const candlePeriodIndex = computed(() => {
-    return selectedPeriodObject.value.index;
-})*/
-
 const lastPoint = computed(() => {
     return store.state.trading.last_point;
-    //return store.getters['trading/lastPoint'];
 });
+
+const changePeriodEvent = (period) => {
+    graph_loading.value = true;
+    store.commit('trading/setGraphPeriod', period);
+    store.dispatch('trading/getGraphFromServer').then(resp => {
+        xData = resp.candlesData;
+        yData = resp.volumeData;
+        hcInstance.value.charts[0].series.forEach((item, i) => {
+            const id = _.get(item, 'options.id', undefined);
+            if (['main','highcharts-navigator-series'].indexOf(id) !== -1) {
+                hcInstance.value.charts[0].series[i].setData(xData, false, false, false);
+            }
+        });
+        hcInstance.value.charts[0].series[1].setData(yData, false);
+        if (xData.length > valuesToDisplay)
+        {
+            hcInstance.value.charts[0].xAxis[0].setExtremes(xData[xData.length - valuesToDisplay - 1].x, xData[xData.length - 1].x);
+        }
+        else
+        {
+            hcInstance.value.charts[0].xAxis[0].setExtremes(xData[0].x, xData[xData.length - 1].x);
+        }
+        graph_loading.value = false;
+    });
+    return false;
+};
+
+const intervals = [
+    { index: 0, room: 1, text: '1m', type: 'minute', count: valuesToDisplay, events: { click: () => changePeriodEvent('1m') }},
+    { index: 1, room: 5, text: '5m', type: 'minute', count: valuesToDisplay * 5, events: { click: () => changePeriodEvent('5m') }},
+    { index: 2, room: 15, text: '15m', type: 'minute', count: valuesToDisplay * 15, events: { click: () => changePeriodEvent('15m') }},
+    { index: 3, room: 30, text: '30m', type: 'minute', count: valuesToDisplay * 30, events: { click: () => changePeriodEvent('30m') }},
+    { index: 4, room: 60, text: '1h', type: 'hour', count: valuesToDisplay, events: { click: () => changePeriodEvent('1h') }},
+    { index: 5, room: 240, text: '4h', type: 'hour', count: valuesToDisplay * 4, events: { click: () => changePeriodEvent('4h') }},
+    { index: 6, room: 1440, text: '1d', type: 'day', count: valuesToDisplay, events: { click: () => changePeriodEvent('1d') }},
+    { index: 7, room: 10080, text: '1w', type: 'day', count: valuesToDisplay * 7, events: { click: () => changePeriodEvent('1w') }},
+];
 
 watch(
     () => lastPoint.value,
@@ -199,174 +210,16 @@ let options = {
                 }
             }
         },
-        buttons: [
-            {
-                type: 'minute',
-                index: 0,
-                room: 1,
-                count: 97,
-                text: '1m',
-                events: {
-                    click: e => {
-                        graph_loading.value = true;
-                        candle_period = '1m';
-                        store.commit('trading/setGraphPeriod', candle_period);
-                        store.dispatch('trading/getGraphFromServer').then(resp => {
-                            xData = resp.candlesData;
-                            yData = resp.volumeData;
-                            hcInstance.value.charts[0].series.forEach((item, i) => {
-                                const id = _.get(item, 'options.id', undefined);
-                                if (['main','highcharts-navigator-series'].indexOf(id) !== -1) {
-                                    hcInstance.value.charts[0].series[i].setData(xData, false, false, false);
-                                }
-                            });
-                            hcInstance.value.charts[0].series[1].setData(yData, false);
-                            if (xData.length > valuesToDisplay)
-                            {
-                                hcInstance.value.charts[0].xAxis[0].setExtremes(xData[xData.length - valuesToDisplay - 1].x, xData[xData.length - 1].x);
-                            }
-                            else
-                            {
-                                hcInstance.value.charts[0].xAxis[0].setExtremes(xData[0].x, xData[xData.length - 1].x);
-                            }
-                            graph_loading.value = false;
-                        });
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'minute',
-                index: 1,
-                room: 5,
-                count: 97 * 5,
-                text: '5m',
-                events: {
-                    click: e => {
-                        graph_loading.value = true;
-                        candle_period = '5m';
-                        store.commit('trading/setGraphPeriod', candle_period);
-                        store.dispatch('trading/getGraphFromServer').then(resp => {
-                            xData = resp.candlesData;
-                            yData = resp.volumeData;
-                            hcInstance.value.charts[0].series.forEach((item, i) => {
-                                const id = _.get(item, 'options.id', undefined);
-                                if (['main','highcharts-navigator-series'].indexOf(id) !== -1) {
-                                    hcInstance.value.charts[0].series[i].setData(xData, false, false, false);
-                                }
-                            });
-                            hcInstance.value.charts[0].series[1].setData(yData, false);
-                            if (xData.length > valuesToDisplay)
-                            {
-                                hcInstance.value.charts[0].xAxis[0].setExtremes(xData[xData.length - valuesToDisplay - 1].x, xData[xData.length - 1].x);
-                            }
-                            else
-                            {
-                                hcInstance.value.charts[0].xAxis[0].setExtremes(xData[0].x, xData[xData.length - 1].x);
-                            }
-                            graph_loading.value = false;
-                        });
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'minute',
-                index: 2,
-                room: 15,
-                count: 97 * 15,
-                text: '15m',
-                events: {
-                    click: () => {
-                        candle_period = '15m';
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'minute',
-                index: 3,
-                room: 30,
-                count: 97 * 30,
-                text: '30m',
-                events: {
-                    click: () => {
-                        candle_period = '30m';
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'hour',
-                index: 4,
-                room: 60,
-                count: 97,
-                text: '1h',
-                events: {
-                    click: () => {
-                        candle_period = '1h';
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'hour',
-                index: 5,
-                room: 240,
-                count: 97 * 4,
-                text: '4h',
-                events: {
-                    click: () => {
-                        candle_period = '4h';
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'day',
-                index: 6,
-                room: 1440,
-                count: 97,
-                text: '1d',
-                events: {
-                    click: () => {
-                        candle_period = '1d';
-                        return false;
-                    },
-                },
-            },
-            {
-                type: 'day',
-                index: 7,
-                room: 10080,
-                count: 97 * 7,
-                text: '1w',
-                events: {
-                    click: () => {
-                        candle_period = '1w';
-                        return false;
-                    },
-                },
-            },
-        ],
+        buttons: intervals,
         selected: 0,
         inputEnabled: false,
     },
     xAxis: {
-        overscroll: 1 * 60 * 1000, // изначально одна минута
+        overscroll: 60 * 1000, // изначально одна минута
         resize: {
             enabled: false,
         },
-        range: valuesToDisplay * 1 * 60 * 1000, // изначально данные показываем за 97 минут
-        events: {
-            /*setExtremes: _.debounce(e => {
-                console.log(e.min);
-                console.log(e.max);
-                console.log(e.trigger);
-                hcInstance.value.charts[0].redraw();
-                hcInstance.value.charts[0].reflow();
-            }, 100),*/
-        },
+        range: valuesToDisplay * 60 * 1000, // изначально данные показываем за 97 минут
     },
     yAxis: [
         {
@@ -478,35 +331,6 @@ let options = {
         },
     },
 };
-
-/*const initGraphDataHandler = () => {
-    const data = {
-        xData: store.state.trading.candlesData,
-        yData: store.state.trading.volumeData,
-    };
-    //const candle = this.$refs.candle;
-    const graphsToProcess = ['main'];
-    hcInstance.value.charts[0].series.forEach((item, i) => {
-        const id = _.get(item, 'options.id', undefined);
-        if (graphsToProcess.indexOf(id) !== -1) {
-            hcInstance.value.charts[0].series[i].setData(data.xData, false, false, false);
-        }
-    });
-    hcInstance.value.charts[0].series[1].setData(data.yData, false);
-    const overscroll = Math.round(candle_room.value) * 60 * 1000;
-    hcInstance.value.charts[0].xAxis[0].update({
-        range: valuesToDisplay * candle_room.value * 60 * 1000,
-        overscroll,
-    });
-    if (data.xData.length > valuesToDisplay)
-        hcInstance.value.charts[0].xAxis[0].setExtremes(data.xData[data.xData.length - valuesToDisplay - 1].x, data.xData[data.xData.length - 1].x);
-    else
-        hcInstance.value.charts[0].xAxis[0].setExtremes(data.xData[0].x, data.xData[data.xData.length - 1].x);
-    setTimeout(() => {
-        hcInstance.value.charts[0].redraw();
-        hcInstance.value.charts[0].reflow();
-    }, 200);
-};*/
 
 onMounted(() => {
     store.commit('trading/setGraphPeriod', candle_period);
